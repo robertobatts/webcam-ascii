0eb314b270c183c668f962a21672f42fe6c5968a
Roberto Battaglia <battaroberto@gmail.com>
2022-06-18 16:40:21 +0200

    use div instead of canvas

--- src/App.js
+++ src/App.js
@@ -9,39 +9,34 @@ function App() {
   useEffect(() => {
     if (myP5 === undefined) {
       const sketch = (p) => {
-        const density = "¶@ØÆMåBNÊßÔR#8Q&mÃ0À$GXZA5ñk2S%±3Fz¢yÝCJf1t7ªLc¿+?(r/¤²!*;\"^:,'.`  ";
+        const density = "¶@ØÆMåBNÊßÔR#8Q&mÃ0À$GXZA5ñk2S%±3Fz¢yÝCJf1t7ªLc¿+?(r/¤²!*;\"^:,'.` ";
         let image;
         p.preload = () => {
-          image = p.loadImage("./pepa.jpg");
+          image = p.loadImage("./dog.png");
         }
 
         p.setup = () => {
-          p.createCanvas(800, 800);
-        }
+          p.noCanvas();
 
-        p.draw = () => {
           p.background(0);
           //p.image(image, 0, 0, p.width, p.height);
 
           let w = p.width / image.width;
           let h = p.height / image.height;
           image.loadPixels();
-          for (let i = 0; i < image.width; i++) {
-            for (let j = 0; j < image.height; j++) {
+          for (let j = 0; j < image.height; j++) {
+            let row = '';
+            for (let i = 0; i < image.width; i++) {
               const pixelIndex = (i + j * image.width) * 4;
               const r = image.pixels[pixelIndex];
               const g = image.pixels[pixelIndex + 1];
               const b = image.pixels[pixelIndex + 2];
               const avg = (r + g + b) / 3;
               const charIndex = p.floor(p.map(avg, 0, 255, density.length, 0));
-              p.noStroke();
-              p.fill(255);
-              p.textSize(w);
-              p.textAlign(p.LEFT, p.TOP);
-              p.text(density.charAt(charIndex), i * w, j * h);
-            
-
+              const char = density.charAt(charIndex);
+              row += char === ' ' ? '&nbsp' : char;
             }
+            p.createDiv(row);
           }
         }
       }
@@ -52,7 +47,8 @@ function App() {
 
 
   return (
-    <div className='App' ref={sketchRef}>
+    <div className='App App-header'>
+      <div className='image-container' ref={sketchRef} />
     </div>
   );
 }
