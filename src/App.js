387fc2bf8bfa2ccb3a55f134eb91bc3933dcc8e7
Roberto Battaglia <battaroberto@gmail.com>
2022-06-18 14:33:38 +0200

    init react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        Hello
+      </div>
+    </div>
+  );
+}
+
+export default App;
